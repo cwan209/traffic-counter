da1ef5b2b608f0049b6a46d46591b36c40a3a026
Luke Wang <luke.wang@myob.com>
2023-03-26 10:28:37 +1100

    constructed dependecies in the main class and added input file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Printer consolePrinter = new ConsolePrinter();
+
+        TrafficLogParser trafficLogParser = new TrafficLogParser();
+        TrafficLogAnalyzer trafficLogAnalyzer = new TrafficLogAnalyzer();
+        Outputer outputer = new Outputer(consolePrinter);
+        ArgsParser argsParser = new ArgsParser();
+
+        TrafficCounter trafficCounter = new TrafficCounter(trafficLogParser, trafficLogAnalyzer, outputer, argsParser);
+        trafficCounter.run(args);
     }
 }
